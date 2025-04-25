6def898577c58664e49e1efce9594664f60c6198
FismanMaxim <mfisman@mts.ru>
2025-04-25 14:46:25 +0300

    move dependencies, add analyze and format comment

--- lib/presentation/screens/main_screen.dart
+++ lib/presentation/screens/main_screen.dart
@@ -6,7 +6,6 @@ import 'package:tinder_cats/domain/cat_service.dart';
 import 'package:tinder_cats/domain/liked_cats_service.dart';
 import 'package:tinder_cats/presentation/components/like_button.dart';
 import 'package:tinder_cats/presentation/screens/details_screen.dart';
-import 'package:get_it/get_it.dart';
 import 'package:tinder_cats/utils/network_utils.dart';
 
 import 'liked_cats_screen.dart';
